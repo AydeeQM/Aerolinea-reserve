7d899efc2221da53961f4228ae69057c2d1b3d47
aydee <aydee.qm@gmail.com>
2017-09-23 16:45:56 -0500

    generando un imput show

--- assets/js/linea.js
+++ assets/js/linea.js
@@ -1,34 +1,45 @@
 "use strict";
 
 class Reserve {
-    constructor(numSeat, row){
+    constructor(numSeat, row) {
         this.numSeat = numSeat;
         this.row = row;
         this.passengers = [];
         this.currentCell = undefined;
     }
-    
-    drawSeat () {
-        let col = this.numSeat/ this.row;
+
+    drawSeat() {
+        let col = this.numSeat / this.row;
         let passage = '';
         let table = '';
         table += `<table class="table table-bordered text-center">`;
-        for(let i = 1; i <= this.row; i++){
-            table +=  `<tr>`;
+        for (let i = 1; i <= this.row; i++) {
+            table += `<tr>`;
             let group_seat = i;
-            for(let j = 0; j < col; j++){
+            for (let j = 0; j < col; j++) {
                 table += `<td>${group_seat}</td>`;
-                group_seat +=6;
+                group_seat += 6;
             }
 
-            (i==3)? passage += `<td></td>`: passage = '';
+            (i == 3) ? passage += `<td></td>` : passage = '';
             table += `</tr>${passage}`;
         }
 
         table += `</table>`;
         $('#tablero').append(table);
     }
+
+    showInput() {
+        $('#addpass').click(() => {
+            $('#passenger').show();
+            $('#send').click(() => {
+                $('#passenger').hide();
+            });
+        });
+
+    }
 }
 
-let aeroline_reserve = new Reserve(30,6);
+let aeroline_reserve = new Reserve(30, 6);
 aeroline_reserve.drawSeat();
+aeroline_reserve.showInput();
