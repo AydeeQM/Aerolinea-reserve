4b766eddc1c6c1108c76ff5bbb7777840e2974bc
aydee <aydee.qm@gmail.com>
2017-09-23 15:58:50 -0500

    generando asientos en la aerolinea

---
+++ assets/js/linea.js
@@ -0,0 +1,34 @@
+"use strict";
+
+class Reserve {
+    constructor(numSeat, row){
+        this.numSeat = numSeat;
+        this.row = row;
+        this.passengers = [];
+        this.currentCell = undefined;
+    }
+    
+    drawSeat () {
+        let col = this.numSeat/ this.row;
+        let passage = '';
+        let table = '';
+        table += `<table class="table table-bordered text-center">`;
+        for(let i = 1; i <= this.row; i++){
+            table +=  `<tr>`;
+            let group_seat = i;
+            for(let j = 0; j < col; j++){
+                table += `<td>${group_seat}</td>`;
+                group_seat +=6;
+            }
+
+            (i==3)? passage += `<td></td>`: passage = '';
+            table += `</tr>${passage}`;
+        }
+
+        table += `</table>`;
+        $('#tablero').append(table);
+    }
+}
+
+let aeroline_reserve = new Reserve(30,6);
+aeroline_reserve.drawSeat();
