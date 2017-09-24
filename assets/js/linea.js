9b8f64afbda9ef9d5f582369a6745c85be766bc6
aydee <aydee.qm@gmail.com>
2017-09-23 20:44:17 -0500

    funcionaliad en agregar, buscar y listar

--- assets/js/linea.js
+++ assets/js/linea.js
@@ -36,11 +36,11 @@ class Reserve {
             });
             return false;
         });
-        /*$('#send').click(() => {
-                    $('#passenger').animate({ 'top': '-300px' }, 500, () => {
-                        $('#overlay').fadeOut('fast');
-                    });
-                }); */
+        $('#send').click(() => {
+            $('#passenger').animate({ 'top': '-300px' }, 500, () => {
+                $('#overlay').fadeOut('fast');
+            });
+        });
         $('#boxclose').click(() => {
             $('#passenger').animate({ 'top': '-300px' }, 500, () => {
                 $('#overlay').fadeOut('fast');
@@ -50,19 +50,24 @@ class Reserve {
 
     inboxUser() {
         $('table tr td').click((event) => {
+
             $("#numseat").val(parseInt(event.target.textContent));
-            let num = parseInt($('#numseat').text());//numero del asiento
-            this.currentCell = $(event.target);//recupera el TD actual
-
-            //recorre el array this.passengers
-            $.grep(this.passengers, (value, index) => {
-                if (num == this.passengers[index].Item) {
-                    $("#nombre").val(this.passengers[index].Nombre);
-                    $("#apell").val(this.passengers[index].Apellido);
-                    $("#eldni").val(this.passengers[index].Dni);
-                }
-            });
-
+            let num = parseInt($('#numseat').val());
+            this.currentCell = $(event.target);
+            //this.currentCell.css('background', '#C6EBEB');
+            /* $.grep(this.passengers, (value, index) => {
+                    if (num == this.passengers[index].Item) {
+                        $('#tarjeta').append(`Asiento N°: ${this.passengers[index].Item}<br>\
+                                            Nombre: ${this.passengers[index].Nombre}<br>\
+                                            Apellido: ${this.passengers[index].Apellido}<br>\
+                                            DNI N°: ${this.passengers[index].Dni}<br><br>`)
+                    }
+                });
+    
+                $('#overlay').fadeIn(200, () => {
+                    $('#modal').animate({ 'top': '50px' }, 200);
+                });
+                return false; */
         });
     }
     addUser() {
@@ -80,12 +85,53 @@ class Reserve {
             this.passengers.push(data);
             alert('El pasajero ' + ' ' + data.Nombre + ' ' + data.Apellido + ' en el asiento N° ' + data.Item + ' se ha agregado correctamente');
             this.currentCell.css('background', '#F8ED50');
-            $("#nombre").val('');
-            $("#apell").val('');
-            $("#eldni").val('');
+
+            $("#numseat").val('');
+            $("#name").val('');
+            $("#surname").val('');
+            $("#iddni").val('');
 
         });
     }
+
+    searchList() {
+        $('#search').click(() => {
+            $('#overlay').fadeIn(200, () => {
+                $('#search_id').animate({ 'top': '50px' }, 200);
+            });
+            return false;
+        });
+
+        $('#buscar').click(() => {
+            let id_dni = parseInt($('#get_dni').val());
+            $.grep(this.passengers, (value, index) => {
+                if (id_dni == this.passengers[index].Dni) {
+                    $('#target').append(`Asiento N°: ${this.passengers[index].Item}<br>\
+                                        Nombre: ${this.passengers[index].Nombre}<br>\
+                                        Apellido: ${this.passengers[index].Apellido}<br>\
+                                        DNI N°: ${this.passengers[index].Dni}<br><br>`)
+                }
+            });
+        });
+
+        $('#boxdelete').click(() => {
+            $('#search_id').animate({ 'top': '-300px' }, 500, () => {
+                $('#overlay').fadeOut('fast');
+            });
+        });
+    }
+
+    print_All() {
+        $('#print').click(() => {
+            for (let i = this.passengers.length - 1; i < this.passengers.length; i++) {
+                $("#records").append(`Asiento N°: ${this.passengers[i].Item}<br>\
+                                Nombre: ${this.passengers[i].Nombre}<br>\
+                                Apellido: ${this.passengers[i].Apellido}<br>\
+                                DNI N°: ${this.passengers[i].Dni}<br><br>`);
+            };
+        });
+    }
+
 }
 
 let aeroline_reserve = new Reserve(30, 6);
@@ -93,3 +139,5 @@ aeroline_reserve.drawSeat();
 aeroline_reserve.showInput();
 aeroline_reserve.inboxUser();
 aeroline_reserve.addUser();
+aeroline_reserve.searchList();
+aeroline_reserve.print_All();
